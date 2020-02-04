549ee82ab94563ab3cd8a51476c7036a60584de3
Eric <onlyeric23@gmail.com>
2020-02-04 10:15:15 +0800

    Minor changes

---
+++ functions/src/handlers/index.ts
@@ -0,0 +1,5 @@
+export * from './backup';
+export * from './transform';
+export * from './hello';
+export * from './graphql';
+export * from './geocode';
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,6 @@
 // tslint:disable:no-submodule-imports
 import 'source-map-support/register';
 
-export * from './handlers/backup';
-export * from './handlers/transform';
-export * from './handlers/hello';
-export * from './handlers/graphql';
-export * from './handlers/geocode';
-
+export * from './handlers';
 export * from './schedule';
-
 export * from './migrates';
