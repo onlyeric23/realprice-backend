2bb7db93d1244159f2f19c3f8c76ac78ce96e33d
Eric <onlyeric23@gmail.com>
2020-04-01 13:24:00 +0800

    Functions for mailing

--- functions/src/core/mail.ts
+++ functions/src/core/mail.ts
@@ -3,20 +3,45 @@ import { inspect } from 'util';
 import { SENDGRID_API_KEY } from '../../config/config';
 import { ADMIN_EMAIL } from './constant';
 
-export const notifyException = ({
-  text,
-  html,
-}: {
+interface NotificationOptions {
   text?: string;
   html?: string;
-}) => {
+  subject: string;
+  from: string;
+}
+
+export const notify = ({ text, html, subject, from }: NotificationOptions) => {
   sgMail.setApiKey(SENDGRID_API_KEY!);
   const msg = {
     to: ADMIN_EMAIL,
-    from: 'error@realprice.com.tw',
-    subject: 'Exception notification',
+    from,
+    subject,
     text,
     html,
   };
   return sgMail.send(msg).catch(error => console.error(inspect(error)));
+};
+
+export const notifyException = ({
+  text,
+  html,
+}: Omit<NotificationOptions, 'subject' | 'from'>) => {
+  return notify({
+    text,
+    html,
+    subject: 'Exception notification',
+    from: 'error@realprice.com.tw',
+  });
+};
+
+export const notifyDebugging = ({
+  text,
+  html,
+}: Omit<NotificationOptions, 'subject' | 'from'>) => {
+  return notify({
+    text,
+    html,
+    subject: 'Debugging notification',
+    from: 'debug@realprice.com.tw',
+  });
 };
